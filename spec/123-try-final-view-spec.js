f97fffb46541eb77875026fbce57341dfc9083b6
qwerty88T <guiongblacksire@gmail.com>
2022-10-03 12:34:01 +0800

    aaaa

--- spec/123-try-final-view-spec.js
+++ spec/123-try-final-view-spec.js
@@ -1,9 +1,3 @@
 'use babel';
 
 import 123TryFinalView from '../lib/123-try-final-view';
-
-describe('123TryFinalView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
